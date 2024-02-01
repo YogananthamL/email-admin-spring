3bd7c8683f4f5a24bf8e24f3a5901f9da76241d7
Yoganantham L <lsaran825@gmail.com>
2024-02-01 19:42:05 +0530

    new spring project

---
+++ src/main/java/com/spring/emailadmin/EmailAdminApplication.java
@@ -0,0 +1,13 @@
+package com.spring.emailadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/emailadmin/EmailAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.emailadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
